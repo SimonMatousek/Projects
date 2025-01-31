f98059bb48d238c136c4239dde89aef2d0fc0aaa
Simon Matousek <simon.matousek09@gmail.com>
2025-01-31 13:59:56 +0100

    files added

--- cli/apps/encryption/password_strengh_cli.py
+++ cli/apps/encryption/password_strengh_cli.py
@@ -1,8 +1,50 @@
-"""
-What is a strong password:
--at least 6 characters... -> read about it
--Letters (a - A)
--Numbers(0-9)
--does not repeat characters
-"""
+#What is a strong password:
+#-at least 12 characters - Done
+#-Letters (a - A) - Done
+#-Numbers(0-9) - Done
+#-TODO: does not repeat characters
+
+
+class Check_password_strength:
+    def __init__(self):
+        self.password: str = input("Input the password to check: ")
+        
+    def check_if_repeats(self, password: str) -> bool:
+        for size in range(3, int(len(password)/2)+1):
+            for index in range(len(password) - size * 2 + 1):
+                if password[index : index + size] == password[index + size : index + size * 2]:
+                    return True
+        return False
+    
+    def all_types_present(self, password: str) -> bool:
+        lower_case_letters = "abcdefghijklmnopqrstuvwxyz"
+        upper_case_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        numbers = "0123456789"
+        special_characters = "~``!@}#$%^&*()_-+={|[]:';<>,./?"
+        return self.check_str(password, special_characters) and self.check_str(password, lower_case_letters) and self.check_str(password, upper_case_letters) and self.check_str(password, numbers)
+    
+    def check_str(self, main_string: str, search_string: str) -> bool:
+        contains: bool = False
+        for c in main_string:
+            for s in search_string:
+                if c == s:
+                    contains = True
+                    exit
+        return contains
+    
+    def check_strength(self) -> str:
+        if not len(self.password) > 11:
+            return "The password is too short."
+        if not self.all_types_present(self.password):
+            return "The password needs to contain upper and lowercase letters, as well as numbers and special characters."
+        if self.check_if_repeats(self.password):
+            return "The password should not repeat."
+        return "The password is strong."
+
+def run():
+    password_strength_checker = Check_password_strength()
+    print(password_strength_checker.check_strength())
+
+if __name__ == "__main__":
+    run()
 
--- cli/apps/encryption/random_password_generator.py
+++ cli/apps/encryption/random_password_generator.py
@@ -9,20 +9,26 @@ from string import ascii_letters, digits, punctuation
     - Punctuation: !"&%$/
     """
 
-def generate_password(length: int) -> str:
-    characters: list = list(ascii_letters + digits + punctuation)
-    shuffle(characters)
-    password_list: list = [choice(characters) for _ in range(length)]
-    return "".join(password_list)
+class Password_generator():
+    def __init__(self):
+        self.password = ""
+
+    def generate(self) -> str:
+        try:
+            length: int = int(input("Please input password length: "))
+        except:
+            return"Something went wrong."
+        if length < 1:
+            return "Only positive password length is accepted"
+        characters: list = list(ascii_letters + digits + punctuation)
+        password_list: list = [choice(characters) for _ in range(length)]
+        return "".join(password_list)
+
 
 def run() -> str:
-    length: int = int(input("Password length: "))
-    password: str = ""
-    if length > 0:
-        return generate_password(length)
-    return "Only positive password length is accepted"
+    password_gen = Password_generator()
+    print(password_gen.generate())
 
 
 if __name__ == "__main__":
-    password = run()
-    print(f"{password=}")
+    run()
